bc9c3142826313075ba168645b15be2332c89978
Saleem Siddique <saleemprogramacion04@gmail.com>
2026-04-20 19:49:17 +0200

    fix(infra): correct psql connection in init scripts and healthchecks
    - add --dbname postgres to init scripts so psql connects to the right db
    - add -d postgres to pg_isready in healthchecks for the same reason

--- infra/init-db/postgres/01-create-databases.sh
+++ infra/init-db/postgres/01-create-databases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
   CREATE DATABASE walletos_users;
   CREATE DATABASE walletos_wallets;
   CREATE DATABASE walletos_notifications;
