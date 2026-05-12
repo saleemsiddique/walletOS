19af5dffd4f757e2b0bcea881f5d46c81939a109
Saleem Siddique <saleemprogramacion04@gmail.com>
2026-05-12 19:44:01 +0200

    feat(wallet-service): estructura de carpetas src/

---
+++ services/wallet-service/src/server.ts
@@ -0,0 +1,27 @@
+import { createApp } from './app';
+import { env } from './config/env';
+import { connectRabbitMQ } from './lib/rabbitmq';
+
+async function start(): Promise<void> {
+  await connectRabbitMQ();
+  const app = createApp();
+
+  const server = app.listen(env.PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`[wallet-service] listening on port ${env.PORT} (${env.NODE_ENV})`);
+  });
+
+  process.on('SIGTERM', () => {
+    server.close(() => process.exit(0));
+  });
+
+  process.on('SIGINT', () => {
+    server.close(() => process.exit(0));
+  });
+}
+
+start().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error('[wallet-service] startup failed:', err);
+  process.exit(1);
+});
