189a615490f1c813c2f8b31224359092805bde45
Saleem Siddique <saleemprogramacion04@gmail.com>
2026-05-30 11:01:13 +0200

    feat(wallet-service): get /wallets/:id/transactions con paginación cursor + tests

--- services/wallet-service/src/controllers/transaction.controller.ts
+++ services/wallet-service/src/controllers/transaction.controller.ts
@@ -1,7 +1,10 @@
 import type { Request, Response, RequestHandler } from 'express';
 import { z } from 'zod';
 import * as transactionService from '../services/transaction.service';
-import { createTransactionSchema } from '../validators/transaction.validators';
+import {
+  createTransactionSchema,
+  listTransactionsSchema,
+} from '../validators/transaction.validators';
 
 const walletIdParamSchema = z.object({ id: z.string().uuid() });
 
@@ -14,4 +17,15 @@ async function handleCreate(req: Request, res: Response): Promise<void> {
 
 export const createTransaction: RequestHandler = (req, res, next) => {
   handleCreate(req, res).catch(next);
+};
+
+async function handleListByWallet(req: Request, res: Response): Promise<void> {
+  const { id: walletId } = walletIdParamSchema.parse(req.params);
+  const query = listTransactionsSchema.parse(req.query);
+  const result = await transactionService.listWalletTransactions(req.userId, walletId, query);
+  res.json(result);
+}
+
+export const listWalletTransactions: RequestHandler = (req, res, next) => {
+  handleListByWallet(req, res).catch(next);
 };
